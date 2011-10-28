5b258daa81837c7af922d0e34e7d92e6366bd8b3
Manish Kumar <manish.kumar@mindfiresolutions.com>
2011-10-28 16:57:45 +0530

    new tpl file for user login and menu block

---
+++ sites/all/themes/tego/user-login.tpl.php
@@ -0,0 +1,7 @@
+<div class="news-heading-title">User Login</div>
+<p>Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget </p>
+<p><?php //print $intro_text; ?></p>
+
+<div class="my-form-wrapper">
+  <?php print $rendered; ?>
+</div>
